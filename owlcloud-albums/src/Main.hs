fafb4632d03918341c0b2b6f8c59668ed27d06d9
Kostiantyn Rybnikov <k-bx@k-bx.com>
2017-11-25 12:53:35 +0200

    Hindent all the code (except for the big API types)

--- owlcloud-albums/src/Main.hs
+++ owlcloud-albums/src/Main.hs
@@ -2,13 +2,13 @@
 
 module Main where
 
-import           Import
-import           Network.HTTP.Client      (Manager, defaultManagerSettings,
-                                           newManager)
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           OwlCloud
-import           Servant
+import Import
+import Network.HTTP.Client
+       (Manager, defaultManagerSettings, newManager)
+import Network.Wai
+import Network.Wai.Handler.Warp
+import OwlCloud
+import Servant
 
 server :: Manager -> Server AlbumsAPI
 server = albums
@@ -21,11 +21,11 @@ app mgr = serve albumsAPI (server mgr)
 
 albums :: Manager -> Maybe SigninToken -> Maybe SortBy -> Handler [Album]
 albums mgr mt sortBy = do
-    checkValidity mgr mt
-    state <- liftIO $ atomically $ readTVar db
-    return (albumsList state)
+  checkValidity mgr mt
+  state <- liftIO $ atomically $ readTVar db
+  return (albumsList state)
 
 main :: IO ()
 main = do
-    mgr <- newManager defaultManagerSettings
-    run 8083 (app mgr)
+  mgr <- newManager defaultManagerSettings
+  run 8083 (app mgr)
--- owlcloud-users/src/Main.hs
+++ owlcloud-users/src/Main.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
 import Control.Monad.Catch (throwM)
-import qualified Data.Set                 as Set
-import qualified Data.UUID                as UUID
-import qualified Data.UUID.V4             as UUID
-import           Import
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           OwlCloud
-import           Servant
+import qualified Data.Set as Set
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
+import Import
+import Network.Wai
+import Network.Wai.Handler.Warp
+import OwlCloud
+import Servant
 
 server :: Server UsersAPI
 server = owlIn :<|> owlOut :<|> tokenValidity
@@ -23,41 +23,42 @@ app :: Application
 app = serve usersAPI server
 
 owlIn :: LoginReq -> Handler SigninToken
-owlIn LoginReq{..} =
-    case (whoo, passwoord) of
-      ("great horned owl", "tiger") -> do
-          uuid <- liftIO UUID.nextRandom
-          let token = SigninToken (UUID.toText uuid)
-          liftIO $ atomically $
-              modifyTVar db $ \s ->
-                s { validTokens = Set.insert token (validTokens s) }
-          return token
-      _ -> throwM (ServantErr 400 "Username/password pair did not match" "" [])
+owlIn LoginReq {..} =
+  case (whoo, passwoord) of
+    ("great horned owl", "tiger") -> do
+      uuid <- liftIO UUID.nextRandom
+      let token = SigninToken (UUID.toText uuid)
+      liftIO $
+        atomically $
+        modifyTVar db $ \s -> s {validTokens = Set.insert token (validTokens s)}
+      return token
+    _ -> throwM (ServantErr 400 "Username/password pair did not match" "" [])
 
 owlOut :: Maybe SigninToken -> Handler ()
 owlOut mt = do
-    checkAuth mt
-    maybe (return ()) out mt
+  checkAuth mt
+  maybe (return ()) out mt
   where
-    out token = liftIO $ atomically $ modifyTVar db $ \s ->
-                  s { validTokens = Set.delete token (validTokens s) }
+    out token =
+      liftIO $
+      atomically $
+      modifyTVar db $ \s -> s {validTokens = Set.delete token (validTokens s)}
 
 tokenValidity :: SigninToken -> Handler TokenValidity
 tokenValidity token = do
-    state <- liftIO $ atomically $ readTVar db
-    return (TokenValidity (Set.member token (validTokens state)))
+  state <- liftIO $ atomically $ readTVar db
+  return (TokenValidity (Set.member token (validTokens state)))
 
 -- Business-logic and utils
-
 checkAuth :: Maybe SigninToken -> Handler ()
 checkAuth = maybe unauthorized runCheck
   where
     runCheck (SigninToken token) = do
-        state <- liftIO $ atomically $ readTVar db
-        let isMember = Set.member (SigninToken token) (validTokens state)
-        unless isMember unauthorized
+      state <- liftIO $ atomically $ readTVar db
+      let isMember = Set.member (SigninToken token) (validTokens state)
+      unless isMember unauthorized
     unauthorized =
-        throwM (ServantErr 401 "You are not authenticated. Please sign-in" "" [])
+      throwM (ServantErr 401 "You are not authenticated. Please sign-in" "" [])
 
 main :: IO ()
 main = run 8082 app
